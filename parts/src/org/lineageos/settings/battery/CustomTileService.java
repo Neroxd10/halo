be9a9e26a43428a30da43834684acfce7c2e5d58
Neroxd10 <neroxd10@gmail.com>
2024-04-29 02:44:30 +0800

    Update GPU Clocks For Legion Assistant

--- parts/src/org/lineageos/settings/battery/CustomTileService.java
+++ parts/src/org/lineageos/settings/battery/CustomTileService.java
@@ -12,6 +12,15 @@ import org.lineageos.settings.utils.FileUtils;
 
 public class CustomTileService extends TileService {
 
+    private static final String MIN_GPU = "/sys/class/kgsl/kgsl-3d0/min_clock_mhz";
+    private static final String MAX_GPU = "/sys/class/kgsl/kgsl-3d0/max_clock_mhz";
+
+    private static final int P_MIN_GPU = 100;
+    private static final int PB_GPU = 439;
+    private static final int BAL_MAX_GPU = 765;
+    private static final int BEAST_GPU = 912;
+
+
     private static final String OPTION_BALANCE_KEY = "option_balance_enable";
     private static final String OPTION_BEASTMODE_KEY = "option_beastmode_enable";
     private static final String OPTION_POWERSAVE_KEY = "option_powersave_enable";
@@ -78,6 +87,7 @@ public class CustomTileService extends TileService {
                 editor.putBoolean(OPTION_BEASTMODE_KEY, false);
                 editor.putBoolean(OPTION_POWERSAVE_KEY, false);
                 updateFrequency(FREQUENCY_BALANCE, FREQUENCY_BALANCE);
+                Gpu(PB_GPU, BAL_MAX_GPU);
                 break;
             case 1:
                 editor.putBoolean(OPTION_BALANCE_KEY, false);
@@ -85,12 +95,14 @@ public class CustomTileService extends TileService {
                 editor.putBoolean(OPTION_POWERSAVE_KEY, false);
                 updateFrequency(FREQUENCY_BEASTMODE_POLICY4, FREQUENCY_BEASTMODE_POLICY7);
                 updateFrequency1(FREQUENCY_MAX_BEASTMODE_POLICY0, FREQUENCY_MAX_BEASTMODE_POLICY4, FREQUENCY_MAX_BEASTMODE_POLICY7);
+                Gpu(BEAST_GPU, BEAST_GPU);
                 break;
             case 2:
                 editor.putBoolean(OPTION_BALANCE_KEY, false);
                 editor.putBoolean(OPTION_BEASTMODE_KEY, false);
                 editor.putBoolean(OPTION_POWERSAVE_KEY, true);
                 updateFrequency(FREQUENCY_POWERSAVE_POLICY4, FREQUENCY_POWERSAVE_POLICY7);
+                Gpu(P_MIN_GPU, PB_GPU);
                 break;
         }
         editor.apply(); // Apply changes to SharedPreferences
@@ -112,6 +124,17 @@ public class CustomTileService extends TileService {
             FileUtils.writeLine(NODE_MIN_POLICY0, String.valueOf(minPolicy0));
             FileUtils.writeLine(NODE_MIN_POLICY4, String.valueOf(minPolicy4));
             FileUtils.writeLine(NODE_MIN_POLICY7, String.valueOf(minPolicy7));
+            Log.d(TAG, "CPU minimum frequency updated successfully");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to update CPU frequency: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void Gpu(int minGPU, int maxGPU) {
+        try {
+            FileUtils.writeLine(MIN_GPU, String.valueOf(minGPU));
+            FileUtils.writeLine(MAX_GPU, String.valueOf(maxGPU));
             Log.d(TAG, "CPU minimum frequency updated successfully");
         } catch (Exception e) {
             Log.e(TAG, "Failed to update CPU frequency: " + e.getMessage());
